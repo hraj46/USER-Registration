7ae9283f62ab5639cfd4944cd49f8ad89e59d875
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-06-30 18:03:18 +0530

    Updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,15 +13,37 @@ const userSchema = new mongoose.Schema({
     },
     // fullname
     fullName: {
-        type: String,
-        required: true,
-        trim: true
-    }
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
     // email
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     // avatar/image
+    avatar: {
+      type: String,  // cloudinary url
+      required: true,
+    },
     // coverPhoto
+    coverImage: {
+      type: String,
+    },
     // password
+    password: {
+      type: String,
+      required: [true, "Password is required"]
+    },
     // refreshToken
+    refreshToken: {
+      type: String,
+    },
 },{
     timestamp: true
 })
